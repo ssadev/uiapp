4b58d530cb605ac3593dc17ee5fb914dbedbedd6
programworld999 <programworld999@gmail.com>
2019-12-27 22:26:45 +0530

    static

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,15 @@
 git status
+echo "Uploading To GitHub"
 git add .
+echo -n "Write Commit For Push:"
 read commitForGit
 git commit -m $commitForGit
+echo "### - Your Code Uploaded To GitHub Successfully - ####"
+
+echo "Uploading To Heroku"
 git push
 git add .
+echo -n "Write Commit For Push:"
 read commitForGit
 git commit -m $commitForGit
 git push heroku master
